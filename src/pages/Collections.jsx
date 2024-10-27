e795623e8a30d39ed541cd40e2df113d6cef125b
Pumplies <maxmax20051120@gmail.com>
2024-10-27 18:49:54 +0600

    adaptiv on collections

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
 import { ProductCard } from "../UI/ProductCard";
 import { Link, useNavigate } from "react-router-dom";
+
 export default function Collections() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
-  const [activeCategory, setActiveCategory] = useState(null); 
+  const [activeCategory, setActiveCategory] = useState(null);
   const [activeIndex, setActiveIndex] = useState(null);
 
-  // ТУТ получаем категории
+  // Получаем категории
   useEffect(() => {
     fetch("http://localhost:4000/api/categories")
       .then((response) => response.json())
@@ -19,21 +20,18 @@ export default function Collections() {
       );
   }, []);
 
-  // А тут товары получаем
+  // Получаем товары
   useEffect(() => {
     fetch("http://localhost:4000/api/products")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
         setProducts(data);
-        setFilteredProducts(data); 
+        setFilteredProducts(data);
       })
-      .catch((error) =>
-        console.error("Ошибка при получении товаров:", error)
-      );
+      .catch((error) => console.error("Ошибка при получении товаров:", error));
   }, []);
 
-  // ТУт выбор категории
+  // Выбор категории
   const handleCategoryClick = (categoryId) => {
     if (categoryId === activeCategory) {
       setActiveCategory(null);
@@ -55,13 +53,12 @@ export default function Collections() {
     setActiveIndex(null);
   };
 
-
   const handleProductClick = (productId) => {
-    const user = localStorage.getItem("user"); 
+    const user = localStorage.getItem("user");
     if (!user) {
-      navigate("/login"); 
+      navigate("/login");
     } else {
-      navigate(`/products/${productId}`); 
+      navigate(`/products/${productId}`);
     }
   };
 
@@ -70,43 +67,51 @@ export default function Collections() {
       <h2 className="font-usuallybold text-[#1F252D] text-[40px] md:text-[160px] mb-8 text-center md:text-left">
         КОЛЛЕКЦИИ
       </h2>
-      <div className="flex flex-col md:flex-row justify-center">
-        <div className="flex flex-col md:w-1/4 gap-5 mb-8 md:mb-0">
-          {categories.map((category, index) => (
-            <span
-              key={category.categoryId}
-              className={`colNavText ${
-                activeIndex === index ? "glitch" : ""
-              } ${activeCategory === category.categoryId ? "active" : ""}`}
-              onClick={() => handleCategoryClick(category.categoryId)}
-              onMouseEnter={() => handleMouseEnter(index)}
-              onMouseLeave={handleMouseLeave}
-            >
-              {category.nameCategory}
-            </span>
-          ))}
-          <div className="flex gap-2 mt-5">
-            <div className="logoCircle bg-white">
-              <img src="/blackLogo.svg" alt="Logo" />
-            </div>
-            <div className="logoCircle bg-[#8229A4]">
-              <img src="/blackLogo.svg" alt="Logo" />
-            </div>
-            <div className="logoCircle bg-[#6D9C5C]">
-              <img src="/blackLogo.svg" alt="Logo" />
-            </div>
-            <div className="logoCircle bg-[#D1C12B]">
-              <img src="/blackLogo.svg" alt="Logo" />
+      <div className="flex flex-col md:flex-row justify-center md:justify-start">
+        <div className="md:absolute">
+          <div className="flex md:flex-col gap-5 mb-8 md:pr-10 sm:pl-24 overflow-x-auto scrollbar-hidden">
+            {categories.map((category, index) => (
+              <span
+                key={category.categoryId}
+                className={`colNavText ${
+                  activeIndex === index ? "glitch" : ""
+                } ${activeCategory === category.categoryId ? "active" : ""}`}
+                onClick={() => handleCategoryClick(category.categoryId)}
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}
+              >
+                {category.nameCategory}
+              </span>
+            ))}
+            <div className="hidden md:flex  gap-2 mt-5">
+              <div className="logoCircle bg-white">
+                <img src="/blackLogo.svg" alt="Logo" />
+              </div>
+              <div className="logoCircle bg-[#8229A4]">
+                <img src="/blackLogo.svg" alt="Logo" />
+              </div>
+              <div className="logoCircle bg-[#6D9C5C]">
+                <img src="/blackLogo.svg" alt="Logo" />
+              </div>
+              <div className="logoCircle bg-[#D1C12B]">
+                <img src="/blackLogo.svg" alt="Logo" />
+              </div>
             </div>
           </div>
         </div>
-        
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-5 gap-x-7 w-full md:w-3/4">
-          {filteredProducts.map((product) => (
-            <div key={product._id} onClick={() => handleProductClick(product._id)} className="cursor-pointer">
-              <ProductCard product={product} />
-            </div>
-          ))}
+
+        <div className="flex w-screen justify-center ">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-5 gap-x-7">
+            {filteredProducts.map((product) => (
+              <div
+                key={product._id}
+                onClick={() => handleProductClick(product._id)}
+                className="cursor-pointer"
+              >
+                <ProductCard product={product} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
