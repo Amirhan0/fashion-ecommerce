b96e2b75ec4a83d854dff95bcb8dacc826b4b792
Pumplies <maxmax20051120@gmail.com>
2024-10-27 18:55:22 +0600

    adaptiv on contactPage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,11 @@
 const ContactPage = () => {
     return (
         <div className="flex flex-col items-end w-full mt-9 mx-auto relative h-full px-4 md:px-0">
-            <h1 className="leading-none text-6xl md:text-9xl noselect text-[#1F252D] font-usuallybold opacity-50 text-right mb-4">
+            <h1 className="leading-none text-5xl md:text-9xl noselect text-[#1F252D] font-usuallybold opacity-50 text-right mb-4">
                 Контакты
             </h1>
             <div className="absolute right-0 top-12">
-                <h1 className="text-5xl md:text-8xl text-white font-uindbase relative z-10 text-right text-shadow">
+                <h1 className="bottom-10 md:bottom-5 text-4xl md:text-8xl text-white font-uindbase relative z-10 text-right text-shadow">
                     КОНТАКТЫ
                 </h1>
             </div>
